cd4902507729663492586ac8813722413bcb4798
wanglei <w_lei@outlook.com>
2013-01-23 18:51:05 +0800

    Initial Commit

---
+++ TestTransition/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  TestTransitionAppController.h
+//  TestTransition
+//
+//  Created by lerry on 13-1-23.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ TestTransition/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  TestTransitionAppController.h
+//  TestTransition
+//
+//  Created by lerry on 13-1-23.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
