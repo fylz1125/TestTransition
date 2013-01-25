ba188b4a5f9e4aaa23dcd25a1b0a20afb4d63b87
wanglei <w_lei@outlook.com>
2013-01-26 00:27:45 +0800

    nothing

--- TestTransition/Classes/MyTransitionScene.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  MyTransitionScene.cpp
-//  TestTransition
-//
-//  Created by lerry on 13-1-23.
-//
-//
-
-#include "MyTransitionScene.h"
--- TestTransition/Classes/dasdafs.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  dasdafs.cpp
-//  TestTransition
-//
-//  Created by lerry on 13-1-23.
-//
-//
-
-#include "dasdafs.h"
